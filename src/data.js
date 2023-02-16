9a3413e7f6021344ada3c521a91c69933b2f9b3c
Ajmera Pavan Kumar <ajmerapavan20@gmail.com>
2023-02-16 22:20:13 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,12 @@
+export const pageLinks = [
+    {id: 1, url: '#home', name: 'home'},
+    {id: 2, url: '#about', name: 'about'},
+    {id: 3, url: '#services', name: 'services'},
+    {id: 4, url: '#tours', name: 'tours'}
+]
+
+export const socialLinks = [
+    {id:1, url: 'https://www.twitter.com', icon: 'fa-brands fa-facebook'},
+    {id:2, url: 'https://www.twitter.com', icon: 'fab fa-twitter'},
+    {id:3, url: 'https://www.twitter.com', icon: 'fab fa-squarespace'}
+]
